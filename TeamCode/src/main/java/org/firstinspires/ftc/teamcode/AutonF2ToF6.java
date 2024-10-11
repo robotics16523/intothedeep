ba68f4d3bf960d017f605c718cc5a8e7404a65cc
EmersonRobotics16523 <robotics12890@emerson-school.org>
2024-10-11 16:57:42 -0400

    We added helper methods for drive forward and drive backward. Coded a box to test auto on sunday 10/13 Maitreyi and Isaac

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonF2ToF6.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public class AutonF2ToParkingZone {
+    /*
+    from finn and isaac: start in F2 and push (individually) the first 3 yellow samples into the net zone
+    then park
+     */
+
+}
