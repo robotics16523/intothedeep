35c1ff6450dd7ad641ac185dde5c22f75aad8025
EmersonRobotics16523 <robotics12890@emerson-school.org>
2024-11-17 16:07:59 -0500

    wrote sample and specimen auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoArmTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoArmTest2.java
@@ -12,9 +12,24 @@ public class AutoArmTest2 extends LinearOpMode {
         waitForStart();
         if (opModeIsActive()) {
             robot.init(hardwareMap);
-            robot.driveForward(30,1);
-           robot.tilter(0.62,1.5);
-
+            robot.grabber(robot.GRABBER_CLOSED);
+            robot.timeOut(0.5);
+            robot.tilter(robot.TILTER_UP,2);
+            robot.timeOut(1);
+            robot.driveForward(robot.FIELD_TILE,0.75);
+            robot.strafeLeft(robot.FIELD_TILE/2,0.75);
+            robot.raiseArm(0.75,1);
+            robot.timeOut(1);
+          robot.driveForward(robot.FIELD_TILE/4.5,0.75);
+            robot.timeOut(1);
+            robot.lowerArm(0.75,0.45);
+           robot.grabber(robot.GRABBER_OPEN);
+           robot.timeOut(1);
+           robot.driveBackward(robot.FIELD_TILE/2,0.75);
+           robot.lowerArm(0.75,0.55);
+           robot.strafeLeft(robot.FIELD_TILE*2,0.75);
+           robot.driveForward(robot.FIELD_TILE*2,0.75);
+           robot.strafeRight(robot.FIELD_TILE,0.75 );
             //  robot.lowerArm(0.5,1.5);
         }
     }
