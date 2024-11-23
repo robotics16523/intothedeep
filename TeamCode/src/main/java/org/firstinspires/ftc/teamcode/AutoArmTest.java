8b102d43945152da9db21d9bd7592420b1f3ffd7
EmersonRobotics16523 <robotics12890@emerson-school.org>
2024-11-22 19:54:53 -0500

    at inspection, adjusted the auton so that it works on this field, scores both samples now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoArmTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="AutoArmTest_v2", group = "Concept")
+@Autonomous(name="AutoArmTest_v6", group = "Concept")
 public class AutoArmTest extends LinearOpMode {
 
     @Override
@@ -12,6 +12,7 @@ public class AutoArmTest extends LinearOpMode {
         waitForStart();
         if (opModeIsActive()) {
             robot.init(hardwareMap);
+
             robot.tilter(robot.TILTER_UP,1);
             robot.timeOut(1);
        robot.strafeRight(58,0.65);
@@ -24,18 +25,19 @@ public class AutoArmTest extends LinearOpMode {
             robot.grabber(robot.GRABBER_OPEN);
           robot.timeOut(.5);
           robot.tilter(robot.TILTER_UP,0.75);
-          robot.driveBackward(20,0.75);
+          robot.driveBackward(16,0.75);
           robot.lowerArm(0.75,2.2);
-          robot.spinRight(74,0.5);
-          robot.driveForward(13,0.5);
+          robot.spinRight(72,0.5);
+          robot.driveForward(15,0.5);
           robot.tilter(robot.TILTER_DOWN,0.75);
           robot.grabber(robot.GRABBER_CLOSED);
           robot.timeOut(.5);
           robot.tilter(robot.TILTER_UP,0.75);
-          robot.spinLeft(80,0.75);
-          robot.driveForward(27.5,0.75);
+          robot.spinLeft(78,0.75);
+          robot.driveForward(28,0.75);
           robot.raiseArm(0.75,2.2);
           robot.tilter(robot.TILTER_MIDDLE,0.75);
+          robot.timeOut(.25);
           robot.grabber(robot.GRABBER_OPEN);
           robot.timeOut(.5);
           robot.tilter(robot.TILTER_UP,0.75);
