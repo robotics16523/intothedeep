83d9d6d9a104b16711d77cd9738332bf010b5b2c
EmersonRobotics16523 <robotics12890@emerson-school.org>
2024-12-10 16:27:46 -0500

    new auton sequence (no time) -finn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoASample2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoASample2.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="AutoASample_v12", group = "Concept")
-public class AutoASample extends LinearOpMode {
+@Autonomous(name="AutoASampleThatHasParking", group = "Concept")
+public class AutoASample2 extends LinearOpMode {
 
     @Override
     public void runOpMode(){
@@ -52,6 +52,12 @@ public class AutoASample extends LinearOpMode {
 //          robot.tilter(0.62,1);
 
           //  robot.lowerArm(0.5,1.5);
+            robot.spinRight(58.50,0.75);
+            robot.driveForward(70,0.75);
+            robot.spinRight(14.18,0.75);
+            robot.driveForward(35,0.75);
+            robot.grabber(robot.GRABBER_CLOSED);
+            robot.tilter(robot.TILTER_DOWN,1);
         }
     }
 }
